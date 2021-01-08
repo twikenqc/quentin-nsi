9d33da3f5f5b6b38458dc349d743a07a909c26eb
quentin <cartronq@gmail.com>
2021-01-08 15:48:48 +0100

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -5,6 +5,8 @@ label=Label(fenetre,text="hello,world")
 label.pack()
 print(hex(31))
 
+
 Bouton_sortie=Button(fenetre,text="quitter",command=fenetre.quit)
 Bouton_sortie.pack()
-fenetre.mainloop()
+
+bouton_couleur=Button(fenetre,text="couleur",command=fenetre
