eee669e30295f092ae5218982895ca9660c19330
quentin <cartronq@gmail.com>
2021-01-08 15:31:09 +0100

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -3,6 +3,8 @@ fenetre = Tk()
 
 label=Label(fenetre,text="hello,world")
 label.pack()
+print(hex(31))
+
 Bouton_sortie=Button(fenetre,text="quitter",command=fenetre.quit)
 Bouton_sortie.pack()
 fenetre.mainloop()
